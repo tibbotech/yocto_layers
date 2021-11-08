51775551cef3299cb30cf4ab4588a23928b11e21
Dvorkin Dmitry <dvorkin@tibbo.com>
2021-11-09 00:47:36 +0300

    spmake: set vars if not defined in the ENV

--- meta-tibbo/recipes-core/spmake/spmake/sp_make.inc.mk
+++ meta-tibbo/recipes-core/spmake/spmake/sp_make.inc.mk
@@ -13,32 +13,32 @@ OUTD=sp_out
 ###### rootfs : there are several of them
 
 ###### *** Xboot (arm926,Chip-B): pre-uboot, bootloader #1
+F_XBT ?= xboot-emmc
 #F_XBT=xboot-nand
-F_XBT=xboot-emmc
 
 ###### xxx No-OS (arm926,Chip-B): main binary [optional]
-F_NON=a926
+F_NON ?= a926-empty
 # a926 firmware ipc_* makes emmc unstable now
-F_NON=a926-empty
+#F_NON=a926
 
 ###### *** U-Boot (A7,Chip-A): bootloader #2
+F_UBT ?= u-boot.bin-a7021_ppg2
 #F_UBT=u-boot-a7021_emmc-sp-r0.img
 #F_UBT=u-boot.img-a7021_nand
 #F_UBT=u-boot.bin-a7021_dsxe
-F_UBT=u-boot.bin-a7021_ppg2
 
 ###### *** DTS
+F_DTB ?= sp7021-ltpp3g2revD.dtb
 #F_DTB=sp7021-ev.dtb
 #F_DTB=sp7021-dsx.dtb
-F_DTB=sp7021-ltpp3g2revD.dtb
 
 ###### xxx Kernel (A7,Chip-A): Linux kernel
-F_KRN=uImage
-# use kernel with initrd for FS repair functions
-F_KRN=uImage-initramfs-tppg2.bin
+# use kernel with initrd for FS repair functions (EMMC)
+F_KRN ?= uImage-initramfs-tppg2.bin
+#F_KRN=uImage
 
 ###### *** rootfs
+F_ROO ?= img-tps-free-tppg2.ext4
 #F_ROO=img-sp-tiny-tppg2.ext4
-F_ROO=img-tps-base-tppg2.ext4
-F_ROO=img-tps-free-tppg2.ext4
+#F_ROO=img-tps-base-tppg2.ext4
 
